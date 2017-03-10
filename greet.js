e70bdcce668a460cc84571481fa9efe55f22f88a
Yanga93 <yangazukelwa@gmail.com>
2017-03-10 11:44:50 +0200

    new updates: bold and literal notation object

--- greet.js
+++ greet.js
@@ -1,37 +1,38 @@
 // declare a global variable//
 var counter = 0;
 var Language = document.getElementsByName("Language");
-// var Inputtext = document.getElementById('Name');
-
+//Add global variable to store the names greeted
+var namesGreeted = {};
+//add  for loop looking if the userName exists in namesGreeted if not increment
+for (var i = 0; i < namesGreeted.length; i++) {}
 // Retrieve
-
 var storedCounter = localStorage.getItem("counter");
 // covert a stored value to be a number
 if (storedCounter) {
   counter = Number(storedCounter)
-  // manipulate your HTML file
+  // manipulate HTML file
   document.getElementById("count").innerHTML = counter;
   // reset line
   var reset = document.getElementById("count");
 }
-
+// creating function greetMe
 function greetMe() {
   var Inputtext = document.getElementById('Name');
-  if (Language[0].checked && Inputtext.value !== "") {
+  if (Language[0].checked && Inputtext.value !== "" && namesGreeted[Inputtext] === undefined) {
     var empty = "";
-    //var Inputtext = document.getElementById('Name');
+    namesGreeted[Inputtext] = 1;
     document.getElementById("demo").innerHTML = "Hello, " +
       Inputtext.value + "!";
     document.getElementById("count").innerHTML = counter += 1;
 
-  } else if (Language[1].checked && Inputtext.value !== "") {
-    var Inputtext = document.getElementById('Name')
+  } else if (Language[1].checked && Inputtext.value !== "" && namesGreeted[Inputtext] === undefined) {
+    namesGreeted[Inputtext] = 1;
     document.getElementById("demo").innerHTML = "Dumela, " +
       Inputtext.value + "!";
     document.getElementById("count").innerHTML = counter += 1;
 
-  } else if (Language[2].checked && Inputtext.value !== "") {
-    var Inputtext = document.getElementById('Name')
+  } else if (Language[2].checked && Inputtext.value !== "" && namesGreeted[Inputtext] === undefined) {
+    namesGreeted[Inputtext] = 1;
     document.getElementById("demo").innerHTML = "Molo, " +
       Inputtext.value + "!";
     document.getElementById("count").innerHTML = counter += 1;
@@ -39,7 +40,8 @@ function greetMe() {
   // Store
   localStorage.setItem("counter", counter);
   Inputtext.value = ""
-}
+};
+
 
 // Reset the button to the value of 0 greetings
 function resetButton() {
@@ -48,3 +50,4 @@ function resetButton() {
   localStorage.setItem("count", 0);
   document.getElementById("count").innerHTML = counter;
 }
+//End of reset button
