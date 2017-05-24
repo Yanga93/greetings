982556928fecfe660b7cc323bf9b9eaf6ed6b417
Yanga93 <yangazukelwa@gmail.com>
2017-05-24 17:31:20 +0200

    clean code changes

--- greet.js
+++ greet.js
@@ -1,70 +1,76 @@
 // declare a global variable//
 var counter = 0;
 var Language = document.getElementsByName("Language");
+var demo = document.getElementById("demo");
+var displayCounter = document.getElementById('displayCounter');
+var greetMeButt = document.getElementById('greetMeButt');
+var resetButt = document.getElementById('resetButt');
+var listOfRadioButtons = document.getElementsByClassName('listOfRadioButtons');
 //Add global variable to store the names greeted
 var namesGreeted = {};
-//add  for loop looking if the userName exists in namesGreeted if not increment
-for (var i = 0; i < namesGreeted.length; i++) {}
+
 // Retrieve
 var storedCounter = localStorage.getItem("counter");
 // covert a stored value to be a number
 if (storedCounter) {
   counter = Number(storedCounter)
+
   // manipulate HTML file
-  document.getElementById("count").innerHTML = counter;
-  // reset line
-  var reset = document.getElementById("count");
+  displayCounter.innerHTML = counter;
+
 }
 // creating function greetMe
-function greetMe() {
+greetMeButt.addEventListener("click", function() {
   var Inputtext = document.getElementById('Name');
-  if (Language[0].checked && namesGreeted[Inputtext] === undefined) {
-    //var empty = "";
+  for (var i = 0; i < Language.length; i++) {
+    var currentBtn = Language[i];
+  if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
     namesGreeted[Inputtext] = Inputtext;
-    document.getElementById("demo").innerHTML = "Hello, " +
+    demo.innerHTML = "Hello, " +
       Inputtext.value + "!";
-    document.getElementById("count").innerHTML = counter += 1;
-    //  counter += 1;
-  } else if (Language[0].checked && namesGreeted[Inputtext] !== undefined) {
-    document.getElementById("demo").innerHTML = "Good to see you again, " +
+    displayCounter.innerHTML = counter += 1;
+  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+    demo.innerHTML = "Good to see you again, " +
       Inputtext.value + "!";
   }
   // end of first if statement
-  else if (Language[1].checked && namesGreeted[Inputtext] === undefined) {
+  else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
     namesGreeted[Inputtext] = 1;
-    document.getElementById("demo").innerHTML = "Dumela, " +
+    demo.innerHTML = "Dumela, " +
       Inputtext.value + "!";
-    document.getElementById("count").innerHTML = counter += 1;
+    displayCounter.innerHTML = counter += 1;
 
-  } else if (Language[1].checked && namesGreeted[Inputtext] !== undefined) {
-    document.getElementById("demo").innerHTML = "Ke thabela ho hobona hape, " +
+  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+    demo.innerHTML = "Ke thabela ho hobona hape, " +
       Inputtext.value + "!";
   }
   // end of first if statement
-  else if (Language[2].checked && namesGreeted[Inputtext] === undefined) {
+  else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
     namesGreeted[Inputtext] = 1;
-    document.getElementById("demo").innerHTML = "Molo, " +
+    demo.innerHTML = "Molo, " +
       Inputtext.value + "!";
-      document.getElementById("count").innerHTML = counter += 1;
+    displayCounter.innerHTML = counter += 1;
 
-  } else if (Language[2].checked && namesGreeted[Inputtext] !== undefined) {
-    document.getElementById("demo").innerHTML = "Kuhle ukubona kwakhona, " +
+  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+    demo.innerHTML = "Kuhle ukubona kwakhona, " +
       Inputtext.value + "!";
 
   }
-  //counter+=1;
-  // end of first if statement
+};
+
   // Store
   localStorage.setItem("counter", counter);
   Inputtext.value = ""
-};
+
+});
 
 
 // Reset the button to the value of 0 greetings
-function resetButton() {
+resetButt.addEventListener("click", function() {
   var count = 0;
   counter = 0;
   localStorage.setItem("count", 0);
-  document.getElementById("count").innerHTML = counter;
-}
+  displayCounter.innerHTML = counter;
+
+});
 //End of reset button
