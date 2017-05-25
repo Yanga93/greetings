1fc46d4889d0c36a2324e6a63272c8b5e9f3cb7e
Yanga93 <yangazukelwa@gmail.com>
2017-05-25 16:44:38 +0200

    refactoring code in progress

--- greet.js
+++ greet.js
@@ -5,9 +5,10 @@ var demo = document.getElementById("demo");
 var displayCounter = document.getElementById('displayCounter');
 var greetMeButt = document.getElementById('greetMeButt');
 var resetButt = document.getElementById('resetButt');
-var listOfRadioButtons = document.getElementsByClassName('listOfRadioButtons');
+
+
 //Add global variable to store the names greeted
-var namesGreeted = {};
+// var namesGreeted = {};
 
 // Retrieve
 var storedCounter = localStorage.getItem("counter");
@@ -19,49 +20,78 @@ if (storedCounter) {
   displayCounter.innerHTML = counter;
 
 }
-// creating function greetMe
-greetMeButt.addEventListener("click", function() {
-  var Inputtext = document.getElementById('Name');
+
+//create test for the radio buttons and must be able to loop throw the list of radio buttons
+function greetMsg(Language) {
   for (var i = 0; i < Language.length; i++) {
     var currentBtn = Language[i];
-  if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
-    namesGreeted[Inputtext] = Inputtext;
-    demo.innerHTML = "Hello, " +
-      Inputtext.value + "!";
-    displayCounter.innerHTML = counter += 1;
-  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
-    demo.innerHTML = "Good to see you again, " +
-      Inputtext.value + "!";
-  }
-  // end of first if statement
-  else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
-    namesGreeted[Inputtext] = 1;
-    demo.innerHTML = "Dumela, " +
-      Inputtext.value + "!";
-    displayCounter.innerHTML = counter += 1;
-
-  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
-    demo.innerHTML = "Ke thabela ho hobona hape, " +
-      Inputtext.value + "!";
-  }
-  // end of first if statement
-  else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
-    namesGreeted[Inputtext] = 1;
-    demo.innerHTML = "Molo, " +
-      Inputtext.value + "!";
-    displayCounter.innerHTML = counter += 1;
-
-  } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
-    demo.innerHTML = "Kuhle ukubona kwakhona, " +
-      Inputtext.value + "!";
-
+    if (currentBtn.checked && currentBtn.value === 'English') {
+      return demo.innerHTML = "Hello, " ;
+    } else if (currentBtn.checked && currentBtn.value === 'Setswana') {
+      return demo.innerHTML = "Dumela, " ;
+    } else {
+      currentBtn.checked && currentBtn.value === 'IsiXosa'
+      return demo.innerHTML = "Molo, " ;
+    }
   }
 };
+var namesGreeted = {}
+function getName() {
+  var Inputtext = document.getElementById('Name').value;
+  for (var i = 0; i < namesGreeted.length; i++) {
+  if (namesGreeted === Inputtext) {
+    return Inputtext;
+    }
+  }
+ (namesGreeted ===) {
 
-  // Store
-  localStorage.setItem("counter", counter);
+  }
+  namesGreeted[i].push(Inputtext)
+}
+
+// creating function greetMe
+greetMeButt.addEventListener("click", function() {
+  greetMsg(Language) + Inputtext.value + "!";
+
   Inputtext.value = ""
+  //   if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
+  //     namesGreeted[Inputtext] = Inputtext;
+  //     demo.innerHTML = "Hello, " +
+  //       Inputtext.value + "!";
+  //     displayCounter.innerHTML = counter += 1;
+  //   } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+  //     demo.innerHTML = "Good to see you again, " +
+  //       Inputtext.value + "!";
+  //   }
+  //   // end of first if statement
+  //   else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
+  //     namesGreeted[Inputtext] = 1;
+  //     demo.innerHTML = "Dumela, " +
+  //       Inputtext.value + "!";
+  //     displayCounter.innerHTML = counter += 1;
+  //
+  //   } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+  //     demo.innerHTML = "Ke thabela ho hobona hape, " +
+  //       Inputtext.value + "!";
+  //   }
+  //   // end of first if statement
+  //   else if (currentBtn.checked && namesGreeted[Inputtext] === undefined) {
+  //     namesGreeted[Inputtext] = 1;
+  //     demo.innerHTML = "Molo, " +
+  //       Inputtext.value + "!";
+  //     displayCounter.innerHTML = counter += 1;
+  //
+  //   } else if (currentBtn.checked && namesGreeted[Inputtext] !== undefined) {
+  //     demo.innerHTML = "Kuhle ukubona kwakhona, " +
+  //       Inputtext.value + "!";
+  //
+  //   }
+  // };
+  //
+  //   // Store
+  //   localStorage.setItem("counter", counter);
 
+  //
 });
 
 
