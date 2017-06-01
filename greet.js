5697adc7b094f0e3cc98532de4a1ccef5db2c992
Yanga93 <yangazukelwa@gmail.com>
2017-06-01 15:00:48 +0200

    decoupled inputtext, radio buttons and languages and called them in one function

--- greet.js
+++ greet.js
@@ -4,27 +4,48 @@ var demo = document.getElementById("demo");
 var displayCounter = document.getElementById('displayCounter');
 var greetMeButt = document.getElementById('greetMeButt');
 var resetButt = document.getElementById('resetButt');
+var Inputtext = document.getElementById('Name');
 
-//create test for the radio buttons and must be able to loop throw the list of radio buttons
-function greetMsg(Language) {
+//create function for the radio buttons and must be able to loop throw the list of radio buttons
+function checkedRadioButton() {
   for (var i = 0; i < Language.length; i++) {
-    var Inputtext = document.getElementById('Name').value;
     var currentBtn = Language[i];
-    if (currentBtn.checked && currentBtn.value === 'English') {
-      return demo.innerHTML = "Hello, " + Inputtext;
-    } else if (currentBtn.checked && currentBtn.value === 'Setswana') {
-      return demo.innerHTML = "Dumela, " + Inputtext;
-    } else {
-      currentBtn.checked && currentBtn.value === 'IsiXosa'
-      return demo.innerHTML = "Molo, " + Inputtext;
-    }
+    if (currentBtn.checked) {
+      return currentBtn.value;
+    };
+  };
+  return "Please make sure language is selected.";
+};
+//create a function for chosenLanguage
+function greetedLanguage(chosenLanguage) {
+
+  if (chosenLanguage === 'English') {
+    return "Hello, ";
+  } else if (chosenLanguage === 'Setswana') {
+    return "Dumela, ";
+  } else if (chosenLanguage === 'IsiXhosa') {
+    return "Molo, ";
+  } else {
+    return "Please make sure language is selected! ";
   }
 };
-// creating function greetMe
+
+//create function to capture the Name Inputtext
+function nameInput() {
+  var nameInput = Inputtext.value;
+  if (nameInput.length > 0) {
+    return Inputtext.value;
+  } else {
+    return "Please make sure your name is entered! ";
+  }
+}
+
+// This function calls all the functionality and displays the results.
 greetMeButt.addEventListener("click", function() {
-  greetMsg(Language);
-  greetCounter();
-  Inputtext.value = ""
+  var checkedLanguage = checkedRadioButton();
+  var greeting = greetedLanguage(checkedLanguage);
+  demo.innerHTML = greeting + nameInput();
+  Inputtext.value = "";
 });
 
 // Reset the button to the value of 0 greetings
--- greetings.counter.js
+++ greetings.counter.js
@@ -1,20 +1,6 @@
+// declare a global variable//
 var counter = 0;
-function greetingCounter() {
-
-  // Retrieve
-  var storedCounter = localStorage.getItem("counter");
-
-  // covert a stored value to be a number
-  if (storedCounter) {
-    counter = Number(storedCounter)
-
-    // manipulate HTML file
-    displayCounter.innerHTML = counter;
-
-    // Store
-    localStorage.setItem("counter", counter);
-}
-
+var Language = document.getElementsByName("Language");
 // Retrieve
 var storedCounter = localStorage.getItem("counter");
 // covert a stored value to be a number
@@ -26,11 +12,45 @@ if (storedCounter) {
   var reset = document.getElementById("count");
 }
 // creating function greetMe
-//  counter += 1;
-function greetMe() {
+function greeCounter() {
+  var Inputtext = document.getElementById('Name');
+  if (Language[0].checked && namesGreeted[Inputtext] === undefined) {
+    //var empty = "";
+    namesGreeted[Inputtext] = Inputtext;
+    document.getElementById("demo").innerHTML = "Hello, " +
+      Inputtext.value + "!";
+    document.getElementById("count").innerHTML = counter += 1;
+    //  counter += 1;
+  } else if (Language[0].checked && namesGreeted[Inputtext] !== undefined) {
+    document.getElementById("demo").innerHTML = "Good to see you again, " +
+      Inputtext.value + "!";
+  }
+  // end of first if statement
+  else if (Language[1].checked && namesGreeted[Inputtext] === undefined) {
+    namesGreeted[Inputtext] = 1;
+    document.getElementById("demo").innerHTML = "Dumela, " +
+      Inputtext.value + "!";
     document.getElementById("count").innerHTML = counter += 1;
 
-  } 
+  } else if (Language[1].checked && namesGreeted[Inputtext] !== undefined) {
+    document.getElementById("demo").innerHTML = "Ke thabela ho hobona hape, " +
+      Inputtext.value + "!";
+  }
+  // end of first if statement
+  else if (Language[2].checked && namesGreeted[Inputtext] === undefined) {
+    namesGreeted[Inputtext] = 1;
+    document.getElementById("demo").innerHTML = "Molo, " +
+      Inputtext.value + "!";
+      document.getElementById("count").innerHTML = counter += 1;
+
+  } else if (Language[2].checked && namesGreeted[Inputtext] !== undefined) {
+    document.getElementById("demo").innerHTML = "Kuhle ukubona kwakhona, " +
+      Inputtext.value + "!";
+
+  }
+  //counter+=1;
+  // end of first if statement
+  // Store
   localStorage.setItem("counter", counter);
   Inputtext.value = ""
 };
